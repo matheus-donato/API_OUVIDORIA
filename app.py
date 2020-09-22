0b111eb58d47787ef9c167d67ac019b80fd2ce04
matheus-donato <donats.donato@gmail.com>
2020-09-22 16:22:56 -0300

    fix subtemas cidadania

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from datetime import timedelta
 import pandas as pd
+import os
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -56,8 +57,17 @@ temas_certo = [
     'Saúde',
     'Urbanística']
 
-
-
+@app.before_first_request
+def create_tables():
+    if os.path.exists('data.db') is False:    
+        db.create_all()
+        #add temas
+        dados_subtemas = pd.read_excel('subtema.xlsx')
+        for tema, tema_certo in zip(temas, temas_certo):
+            Tema(tema, tema_certo).save_to_db()
+        #add subtemas
+        for tema, subtema in zip(dados_subtemas.tema, dados_subtemas.subtema):
+            Subtemas(tema, subtema).save_to_db()
 
 jwt = JWT(app, authenticate, identity) #/auth
 
--- run.py
+++ run.py
@@ -4,6 +4,45 @@ import os
 
 db.init_app(app)
 
+temas = ['ambiente',
+        'cidadania',
+        'civil',
+        'consumidor',
+        'criminal',
+        'deficiencia',
+        'educacao',
+        'eleitoral',
+        'exec_penal',
+        'familia',
+        'idoso',
+        'infa_infra',
+        'infa_n_infra',
+        'mulher',
+        'outros',   
+        'prisional',     
+        'saude',
+        'urbanistica']   
+
+temas_certo = [
+    'Ambiente',
+    'Cidadania',
+    'Civil',
+    'Consumidor',
+    'Criminal',
+    'Deficiência',
+    'Educação',
+    'Eleitoral',
+    'Execução penal',
+    'Família',
+    'Idoso',
+    'Infância infracional',
+    'Infância não infracional',
+    'Mulher',
+    'Outros',
+    'Prisional',
+    'Saúde',
+    'Urbanística']
+
 @app.before_first_request
 def create_tables():
     if os.path.exists('data.db') is False:    
